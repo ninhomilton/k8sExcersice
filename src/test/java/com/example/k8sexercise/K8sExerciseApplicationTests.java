db9685233d51f5874fbf89db806a8de5a0d8a5f6
MiltonOrtegon <milton.ortegon@fitideas.co>
2022-12-20 15:26:29 -0500

    Initial version Hello World

---
+++ src/test/java/com/example/k8sexercise/K8sExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.k8sexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
