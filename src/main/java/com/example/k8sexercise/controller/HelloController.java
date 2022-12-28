3c84f8bec8b049bb7e8bf78ed51d1f44a997aaf2
Milton Ortegon <v-milortegon@expediagroup.com>
2022-12-28 14:59:22 -0500

    New App version

--- src/main/java/com/example/k8sexercise/controller/HelloController.java
+++ src/main/java/com/example/k8sexercise/controller/HelloController.java
@@ -1,14 +1,21 @@
 package com.example.k8sexercise.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping(path = "/sayHello")
-    public String sayHello() {
-        return "Hello world!!!";
+    public String sayHello(@RequestParam String language) {
+        String result = "Hello world!!!";
+        if (language.equalsIgnoreCase("spanish")){
+            result = "Hola mundo!!!";
+        }else if (language.equalsIgnoreCase("french")){
+            result = "Salut monde!!!";
+        }
+        return result;
     }
 
 }
